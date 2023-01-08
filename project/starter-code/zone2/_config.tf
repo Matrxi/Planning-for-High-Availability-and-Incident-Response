e85e7008d7e49429b6726ba1fdad1de451a79d22
Rishabh Aggarwal <rishabhaggarwal005@gmail.com>
2023-01-08 14:30:37 +0530

    Update _config.tf

--- project/starter-code/zone2/_config.tf
+++ project/starter-code/zone2/_config.tf
@@ -1,6 +1,6 @@
 terraform {
    backend "s3" {
-     bucket = "udacity-tf-travis-west"
+     bucket = "udacity-tf-rishabh-west"
      key    = "terraform/terraform.tfstate"
      region = "us-west-1"
    }
